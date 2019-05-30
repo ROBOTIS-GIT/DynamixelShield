72e6eec9df249a1f0b4392406853a08e726664b2
Kei <kkw@robotis.com>
2019-05-30 10:54:31 +0900

    Added new examples.

---
+++ examples/basic/goalCurrent/goalCurrent.ino
@@ -0,0 +1,53 @@
+#include <DynamixelShield.h>
+
+#ifdef ARDUINO_AVR_UNO
+  #include <SoftwareSerial.h>
+  SoftwareSerial soft_serial(10, 11); //RX,TX
+  #define DEBUG_SERIAL soft_serial
+#elif ARDUINO_AVR_MEGA2560
+  #define DEBUG_SERIAL Serial1
+#else
+  #define DEBUG_SERIAL Serial
+#endif
+
+const uint8_t DXL_ID = 1;
+
+DynamixelShield dxl;
+
+void setup() {
+  // put your setup code here, to run once:
+  dxl.begin(1000000);
+  dxl.setPortProtocolVersion(2.0);
+  dxl.ping(DXL_ID);
+
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_CURRENT);
+  dxl.torqueOn(DXL_ID);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   
+  // Please refer to e-Manual(http://emanual.robotis.com) for available range of value. 
+  // Set goalCurrent using RAW unit
+  dxl.setGoalCurrent(DXL_ID, 200);
+  delay(1000);
+  // Print present current
+  DEBUG_SERIAL.print("Present Current(raw) : ");
+  DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID));
+  delay(1000);
+
+  // using mA unit
+  dxl.setGoalCurrent(DXL_ID, 25.8, UNIT_MILLI_AMPERE);
+  delay(1000);
+  DEBUG_SERIAL.print("Present Current(mA) : ");
+  DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID, UNIT_MILLI_AMPERE));
+  delay(1000);
+
+  // using RATIO unit (-100.0 ~ 100.0)
+  dxl.setGoalCurrent(DXL_ID, -10.2, UNIT_RATIO);
+  delay(1000);
+  DEBUG_SERIAL.print("Present Current(ratio) : ");
+  DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID, UNIT_RATIO));
+  delay(1000);
+}
---
+++ examples/basic/goalPWM/goalPWM.ino
@@ -0,0 +1,46 @@
+#include <DynamixelShield.h>
+
+#ifdef ARDUINO_AVR_UNO
+  #include <SoftwareSerial.h>
+  SoftwareSerial soft_serial(10, 11); //RX,TX
+  #define DEBUG_SERIAL soft_serial
+#elif ARDUINO_AVR_MEGA2560
+  #define DEBUG_SERIAL Serial1
+#else
+  #define DEBUG_SERIAL Serial
+#endif
+
+const uint8_t DXL_ID = 1;
+
+DynamixelShield dxl;
+
+void setup() {
+  // put your setup code here, to run once:
+  dxl.begin(1000000);
+  dxl.setPortProtocolVersion(2.0);
+  dxl.ping(DXL_ID);
+
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_PWM);
+  dxl.torqueOn(DXL_ID);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   
+  // Please refer to e-Manual(http://emanual.robotis.com) for available range of value. 
+  // Set goalPWM using RAW unit
+  dxl.setGoalPWM(DXL_ID, 300);
+  delay(1000);
+  // Print present PWM
+  DEBUG_SERIAL.print("Present PWM(raw) : ");
+  DEBUG_SERIAL.println(dxl.getPresentPWM(DXL_ID));
+  delay(1000);
+
+  // using RATIO unit (-100.0 ~ 100.0)
+  dxl.setGoalPWM(DXL_ID, -40.8, UNIT_RATIO);
+  delay(1000);
+  DEBUG_SERIAL.print("Present PWM(ratio) : ");
+  DEBUG_SERIAL.println(dxl.getPresentPWM(DXL_ID, UNIT_RATIO));
+  delay(1000);
+}
